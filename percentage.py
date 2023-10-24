169acc22c8d5f32deccae18c5dc394dbbb5f5f9c
ramm <ram51420@gmil.com>
2023-10-24 22:51:09 +0530

    today my program

---
+++ percentage.py
@@ -0,0 +1,10 @@
+per=int(input("enter your persan percentage -"))
+
+if per>90:
+    print("your Grade A")
+elif per>80 and per<=90:
+    print("Your grade B")
+elif per>60 and per<=80:
+    print("print Your garde C ")
+elif per<60:
+    print("Your garde D")
